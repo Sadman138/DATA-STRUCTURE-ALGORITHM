e09e3450b2f90fa80402c8063887bc807da7dbb8
Fathin Anzum Sadman <ansadman16@gmail.com>
2025-02-21 12:40:03 +0600

    Update Quicksort_alg.cpp

--- Quicksort_alg.cpp
+++ Quicksort_alg.cpp
@@ -1,3 +1,62 @@
+//By First Elements Pivot
+
+#include <iostream>
+using namespace std;
+
+int partition(int a[], int start, int end)
+{
+    int pivot = a[start]; // প্রথম এলিমেন্টকে পিভট ধরা হলো
+    int pos = start; 
+
+    for (int i = start + 1; i <= end; i++) 
+    {
+        if (a[i] < pivot) 
+        {
+            pos++;
+            swap(a[i], a[pos]);
+        }
+    }
+    swap(a[start], a[pos]); // পিভটকে সঠিক স্থানে বসানো হলো
+    return pos; // নতুন পিভট ইনডেক্স রিটার্ন
+}
+
+void quicksort(int a[], int start, int end)
+{
+    if (start >= end)
+        return;
+    int pivot = partition(a, start, end);
+    quicksort(a, start, pivot - 1); // Left part
+    quicksort(a, pivot + 1, end); // Right part
+}
+
+int main()
+{
+    int i, n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter all elements of the array: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    quicksort(a, 0, n - 1);
+    cout << "Sorted array: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+//By Last Elements Pivot
 #include <iostream>
 using namespace std;
 int partition(int a[], int start, int end)
